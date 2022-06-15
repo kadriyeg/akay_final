eb3f37669f07ec8ac23be75e4a91b5afc330637b
kadriyeg <denizk2407@gmail.com>
2022-06-15 11:42:33 +0300

    algoritma page routelandı

---
+++ lib/screens/algoritma_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AlgoritmaPage extends StatelessWidget {
+  const AlgoritmaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Algoritma Dersleri"),
+      ),
+    );
+  }
+}
