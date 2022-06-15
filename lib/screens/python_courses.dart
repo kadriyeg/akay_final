0dfc3cf7ba07b784aa9016bad1ef8b306685e11b
irem <mermer.iremnur@gmail.com>
2022-06-15 18:45:13 +0300

    python courses kaydırma eklendi konular

--- lib/screens/python_courses.dart
+++ lib/screens/python_courses.dart
@@ -19,27 +19,272 @@ class PythonCourses extends StatelessWidget {
           //elevation: 0,
           title: const Text('PYTHON'),
         ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(10.0),
-            width: double.infinity,
-            height: 230,
-            color: buttonColor,
-            child: Stack(
-              children: <Widget> [
-                Positioned(
-                  left: 50,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(12),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 30.0, left: 30.0, right: 30.0),
+          child: Column(
+            
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 8,
+                  itemBuilder: (_, i){
+                    return ElevatedButton(
+                    onPressed: () {  },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                        ),
+                        color: Colors.amber,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(10,10),
+                            blurRadius: 10,
+                            color: Color.fromARGB(255, 15, 6, 24)
+                          )
+                        ]
+                      ),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                                "GİRİŞ",
+                                style: TextStyle(fontSize: 12,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                          Text(
+                                "KONU 1:...",
+                                style: TextStyle(fontSize: 20,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                        ],
+                      ),
+                    ),
+                    ),
+                  );
+
+                })
+              )
+                /*
+                children: [
+                  
+                  //1.konu
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                        ),
+                        color: Colors.amber,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(10,10),
+                            blurRadius: 10,
+                            color: Color.fromARGB(255, 15, 6, 24)
+                          )
+                        ]
+                      ),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                                "GİRİŞ",
+                                style: TextStyle(fontSize: 12,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                          Text(
+                                "KONU 1:...",
+                                style: TextStyle(fontSize: 20,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                        ],
+                      ),
+                    ),
                     ),
                   ),
-                )
-              ],
-            ),
+                  SizedBox(height: 20.0,),
+                  //2.konu
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                        ),
+                        color: Colors.amber,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(10,10),
+                            blurRadius: 10,
+                            color: Color.fromARGB(255, 15, 6, 24)
+                          )
+                        ]
+                      ),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                                "GİRİŞ",
+                                style: TextStyle(fontSize: 12,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                          Text(
+                                "KONU 1:...",
+                                style: TextStyle(fontSize: 20,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                        ],
+                      ),
+                    ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0,),
+                  //3.konu
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                        ),
+                        color: Colors.amber,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(10,10),
+                            blurRadius: 10,
+                            color: Color.fromARGB(255, 15, 6, 24)
+                          )
+                        ]
+                      ),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                                "GİRİŞ",
+                                style: TextStyle(fontSize: 12,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                          Text(
+                                "KONU 1:...",
+                                style: TextStyle(fontSize: 20,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                        ],
+                      ),
+                    ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0,),
+                  //4.konu
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                        ),
+                        color: Colors.amber,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(10,10),
+                            blurRadius: 10,
+                            color: Color.fromARGB(255, 15, 6, 24)
+                          )
+                        ]
+                      ),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                                "GİRİŞ",
+                                style: TextStyle(fontSize: 12,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                          Text(
+                                "KONU 1:...",
+                                style: TextStyle(fontSize: 20,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                        ],
+                      ),
+                    ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  //5.konu
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                        ),
+                        color: Colors.amber,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(10,10),
+                            blurRadius: 10,
+                            color: Color.fromARGB(255, 15, 6, 24)
+                          )
+                        ]
+                      ),
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                                "GİRİŞ",
+                                style: TextStyle(fontSize: 12,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                          Text(
+                                "KONU 1:...",
+                                style: TextStyle(fontSize: 20,color: Colors.white),
+                                textAlign: TextAlign.center,
+                                ),
+                       ],
+                      ),
+                    ),
+                    ),
+                  ),
+                
+                ],*/
+            ],
+          ),
+
           ),
         )
-    );
+      );
   }
 }
