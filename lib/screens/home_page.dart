c5d5a26114469a6049ae3abf68297732482b6e62
irem <mermer.iremnur@gmail.com>
2022-06-15 10:59:52 +0300

    python courses

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/second_page.dart';
+import 'package:flutter_application_1/screens/python_courses.dart';
 import '../constants.dart';
 import 'package:flutter_application_1/main.dart';
 class FirstPage extends StatelessWidget {
--- lib/screens/python_courses.dart
+++ lib/screens/python_courses.dart
@@ -4,13 +4,18 @@ import 'package:flutter_application_1/main.dart';
 import '../constants.dart';
 import '../main.dart';
 
-class SecondPage extends StatelessWidget {
+// ignore: camel_case_types
+class PythonCourses extends StatelessWidget {
   @override
-  const SecondPage({Key? key}) : super(key: key);
+  const PythonCourses({Key? key}) : super(key: key);
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+          backgroundColor: backGroundColor,
+          elevation: 0,
+         //title: const Text('App Title'),
+        ),
     );
   }
 
