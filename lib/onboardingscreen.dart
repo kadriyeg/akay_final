acccfae924677ae673c3383cdc4c876977e1ea5f
Aycoon <aycandemwsw@gmail.com>
2022-06-15 11:58:58 +0300

    onboarding ilk deneme

---
+++ lib/onboardingscreen.dart
@@ -0,0 +1,116 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/material/elevated_button.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen(Set set, {Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemBuilder: (context, index) => const OnboardContent(
+                  image: "images/oturan_akay.png",
+                  title: "introduction 1",
+                  description: 
+                      "Tanıtımın detayları burada olacak, daha az vurgulu görünecek.",
+                ), 
+              ),
+            ),
+            const SizedBox(
+              height: 60, 
+              width: 60,
+              child: ElevatedButton(
+                onPressed: (){
+                  _pageController.nextPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                    );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                ),
+                child: SvgPicture.asset("images/right-arrow-svgrepo-com.svg",
+                 color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardContent extends StatelessWidget {
+  const OnboardContent({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  final String image, title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Spacer(),
+        Image.asset(
+        image,
+        //"images/oturan_akay.png",
+        width: 300,
+        height: 150,
+        fit: BoxFit.fill
+        ),
+        const Spacer(),
+        Text(
+        title,
+        //"introduction 1",
+        textAlign: TextAlign.center,
+        style: Theme.of(context)
+        .textTheme
+        .headline5!
+        .copyWith(fontWeight: FontWeight.w500),
+        //theme of context ile başlayan yer metnin boyutunu ayarlamaya yarar.
+        ),
+        const SizedBox(height: 16),
+        Text(
+        description,
+          //"Tanıtımın detayları burada olacak, daha az vurgulu görünecek.",
+          textAlign: TextAlign.center,
+        ),
+        const Spacer(),
+        
+      ]
+    );
+  }
+}
