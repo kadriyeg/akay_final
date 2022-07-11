e771c954a9659d70c989be438f4b32ab82fb61cc
kadriyeg <denizk2407@gmail.com>
2022-07-11 22:14:08 +0300

    firebase ayarlamaları yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,19 @@ import 'package:flutter_application_1/screens/home_page.dart';
 
 void main() {
   runApp( MyApp());
+ 
 }
 
 class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
-
+  
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
