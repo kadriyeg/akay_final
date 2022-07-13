7d1c762fe47ff5dc1ce0cf383e9433a70dad928c
Aycoon <aycandemwsw@gmail.com>
2022-07-13 15:34:59 +0300

    quiz app first attempts and new filds

---
+++ lib/models/questions_model.dart
@@ -0,0 +1,16 @@
+//creating a question model here
+
+class Question{
+  final String id;
+  final String title;
+  // ignore: non_constant_identifier_names, prefer_typing_uninitialized_variables
+   final Map<String, bool>options;
+
+
+  Question({
+    required this.id,
+    required this.title,
+    required this.options,
+  });
+  
+}
