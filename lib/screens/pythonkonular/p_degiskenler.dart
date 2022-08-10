6cf689219c37cb26376a453bb10f8760c6f55160
Aycoon <aycandemwsw@gmail.com>
2022-08-10 17:04:54 +0300

    python konuları eklendi

--- lib/screens/pythonkonular/p_degiskenler.dart
+++ lib/screens/pythonkonular/p_degiskenler.dart
@@ -5,8 +5,8 @@ import '../python_courses.dart';
 
 class Pdegiskenler extends StatelessWidget {
   const Pdegiskenler({Key? key}) : super(key: key);
-  
-@override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backGroundColor,
@@ -20,36 +20,84 @@ class Pdegiskenler extends StatelessWidget {
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
+              Text(
+                "DEĞİŞKENLER",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              Text(
+                "Program tarafından kullanılacak olan verileri depolamak için bir değişken kullanılır. Bu veriler bir sayı, bir dizi, bir Boolean, bir liste veya başka bir veri türü olabilir. Python’ da bir değişken, ona bir değer atadığınız anda oluşturulur. Atama işlemi bir değeri bir değişken ile eşleştirir. Atama operatörü “=” sembolüdür.\n\n   “X = 10” İfadesinde 10 değeri x değişkenine atanmaktadır.",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              Text(
+                "KOD ÖRNEĞİ",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "X=10;\n\nPrint(x);|\n\nÇıktı: 10",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "X=20;\n\nprint(x);\n\nÇıktı: 20",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              Text(
+                "Çoklu Atama Kod Örneği",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "x, y, z= 100, -45, 0;\n\nprint(“x = ”x, “y= “y, “z= “z);\n\nÇıktı: x= 100 y= -45 z= 0",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              Text(
+                "",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              Text(
+                "",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
               InkWell(
-                      child: Center(
-                        child: Container(
-                          height: 40,
-                          width: 70,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                            color: buttonColor,
-                          ),
-                          child: Padding(
-                          padding:
-                              const EdgeInsets.only(top: 8),
-                          child: Text(
-                            'İleri',
-                            style: TextStyle(fontSize: 16, color: Colors.white),
-                            textAlign: TextAlign.center ,
-                          ),
-                        )
-                        ),
+                child: Center(
+                  child: Container(
+                      height: 40,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: buttonColor,
                       ),
-                       onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return PythonCourses();
-                            },
-                          ),
-                        );
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'İleri',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return PythonCourses();
                       },
                     ),
+                  );
+                },
+              ),
             ],
           ),
         ),
--- lib/screens/pythonkonular/p_pythondasayilar.dart
+++ lib/screens/pythonkonular/p_pythondasayilar.dart
@@ -1,10 +1,128 @@
 import 'package:flutter/material.dart';
 
-class Psayilar  extends StatelessWidget {
-  const Psayilar ({Key? key}) : super(key: key);
+import '../../constants.dart';
+import '../python_courses.dart';
+
+class Psayilar extends StatelessWidget {
+  const Psayilar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backGroundColor,
+      appBar: AppBar(
+        backgroundColor: backGroundColor,
+        elevation: 2,
+        //title: const Text('App Title'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Text(
+                "PYTHONDA SAYILAR",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              Text(
+                "Pythonda kullanılan 2 tane  sayısal veri tipi vardır.\n\nİnteger(tam sayı) : Tam sayıları ifade eder. Örnek olarak; 5, 15, -23 …\n\nFloat(ondalık sayı): Ondalıklı sayıları ifade eder. Örnek olarak; 5.24, 3.14, 5.0 …\n\n",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              Text(
+                "KOD ÖRNEĞİ",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "Print(2+2);\n\nÇıktı: 4",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+                 Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "Print(2+1.5);\n\nÇıktı: 3.5",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              Text(
+                "Matematiksel Operatörler",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+              Text(
+                "Operatörler, değişkenler ve değerler üzerinde işlem yapmak için kullanılır.",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              Text(
+                "	Veri tipi dönüşümleri",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),Text(
+                "“type()” İşlevi  herhangi bir nesnenin veri türünü öğrenmek için kullanılır.",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+                    Text(
+                "KOD ÖRNEĞİ",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "x = 5\n\nprint(type(x))\n\nçıktı: int\n\nVeri türünü dönüştürmek için aşağıdaki yapıcı işlevler kullanılır;\n\nx = int(20)\n\nx = float(20.5)",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              Text(
+                "Dönüşüm için Kod örneği:",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+               Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: buttonColor, width: 2),
+                ),
+                child: Text(
+                  "x = str(3)    # x in türü string olarak ayarlandı '3'\n\ny = int(3)    # y x in türü tam sayı (int) olarak ayarlandı 3\n\nz = float(3)  # z x in türü ondalıklı sayı (float) olarak ayarlandı 3.0",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              InkWell(
+                child: Center(
+                  child: Container(
+                      height: 40,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: buttonColor,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'İleri',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return PythonCourses();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
